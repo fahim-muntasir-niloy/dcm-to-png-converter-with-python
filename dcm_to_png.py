cebfb50886a0a156dafc12d7a346857eebd1e7a0
Fahim Muntasir <64902980+fahim-muntasir-niloy@users.noreply.github.com>
2023-01-24 19:16:19 +0000

    random rename method added

--- dcm_to_png.py
+++ dcm_to_png.py
@@ -1,3 +1,6 @@
+# This file is ready for fully drag and drop into any folder to convert files from dcm to png or jpg format.
+# For jpg just change .png to .jpg in the rename section
+
 import numpy as np
 from PIL import Image
 import pydicom as dcm
@@ -5,7 +8,7 @@ import os
 
 cwd = os.getcwd()
 
-# function to read files one by one
+# function to read files from the directory one by one
 dcmFiles = []
 for dirpath,dirnames, filename in os.walk(cwd):
     # print("current path: ",dirpath)
@@ -18,7 +21,9 @@ for dirpath,dirnames, filename in os.walk(cwd):
 
 # dcmFiles
     
-# convert dcm to jpg
+# Function to convert dcm to jpg
+#* ===================================================>
+
 def dcm_to_jpg(file):
     ds = dcm.dcmread(file)
     newimg = ds.pixel_array.astype(float)
@@ -28,6 +33,25 @@ def dcm_to_jpg(file):
     return finalImage
 
 
+
+#* Rename Section. Use any of the for loops.
+#* ===================================================>
+
+
+
+# This below code will rename files chronologically.
+#* ========================================================>
 for name in dcmFiles:
     image = dcm_to_jpg(name)
-    image.save(str(dcmFiles.index(name)+1) +'.png')
+    image.save(f"{(dcmFiles.index(name)+1)}.png")
+  
+    
+    
+    
+# This below code will rename files giving them random number
+#* ============================================================>
+
+for name in files:
+    new_file_name = round(np.random.rand()*100000000)
+    image = dcm_to_jpg(name)
+    image.save( f"{new_file_name}.png")
